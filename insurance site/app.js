82e766fd535daa72d45d22eb60172d1131ee10a1
Hanik Jain <hanikjain333@gmail.com>
2021-11-12 16:08:53 +0530

    Added start script

--- insurance site/app.js
+++ insurance site/app.js
@@ -33,7 +33,7 @@ app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/user'));
 
 
-
-app.listen(5000, ()=>{console.log('listening on 5000')});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{console.log('listening on 5000')});
 
 
